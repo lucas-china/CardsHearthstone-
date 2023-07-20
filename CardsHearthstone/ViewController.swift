cc6f18cb2fc84f3adb576e40b2fc84edce6bfcaa
Lucas Brito <lucasbrito_9@hotmail.com>
2023-07-20 17:27:05 -0300

    Initial Commit

---
+++ CardsHearthstone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardsHearthstone
+//
+//  Created by Lucas Santana Brito on 20/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
