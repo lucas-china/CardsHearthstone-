5f7db399eefce2bcc8dfbaffaae3cfe45a75599e
Lucas Brito <lucasbrito_9@hotmail.com>
2023-07-20 18:08:07 -0300

    Created a CardListWorker.

---
+++ CardsHearthstone/Scenes/CardList/Workers/CardListWorker.swift
@@ -0,0 +1,32 @@
+//
+//  CardListWorker.swift
+//  CardsHearthstone
+//
+//  Created by Lucas Santana Brito on 20/07/23.
+//
+
+import Foundation
+
+typealias CardListResponse = [CardListModels.Cards.Response]
+
+protocol CardListWorkerLogic: AnyObject {
+    func getCardList(completionHandler: @escaping (Result<CardListResponse, NetworkError>) -> Void)
+}
+
+final class CardListWorker {
+    private let service: NetworkManagerProtocol
+    
+    init(service: NetworkManagerProtocol = NetworkManager()) {
+        self.service = service
+    }
+}
+
+extension CardListWorker: CardListWorkerLogic {
+    func getCardList(completionHandler: @escaping (Result<CardListResponse, NetworkError>) -> Void) {
+        service.performDataTask(route: .cards, httpMethod: .get) { (result: Result<CardListResponse, NetworkError>) in
+            DispatchQueue.main.async {
+                completionHandler(result)
+            }
+        }
+    }
+}
