1d9c8996b734fe2bfcd3cd15f8391eadd8587c00
Lucas Brito <lucasbrito_9@hotmail.com>
2023-07-20 17:54:24 -0300

    Created CardListModels

---
+++ CardsHearthstone/Scenes/CardList/Models/CardListModels.swift
@@ -0,0 +1,33 @@
+//
+//  CardListModels.swift
+//  CardsHearthstone
+//
+//  Created by Lucas Santana Brito on 20/07/23.
+//
+
+import Foundation
+
+enum CardListModels {
+    enum Cards {
+        struct Request {}
+        struct Response: Decodable {
+            let id:String
+            let name:String?
+            let text:String?
+            let flavor:String?
+            let artist:String?
+            let attack:Int?
+            let cardClass:String?
+            let collectible:Bool?
+            let cost:Int?
+            let elite:Bool?
+            let faction:String?
+            let health:Int?
+            let rarity:String?
+            let set:String?
+            let type:String?
+            let mechanics: [String]?
+        }
+        struct ViewModel {}
+    }
+}
